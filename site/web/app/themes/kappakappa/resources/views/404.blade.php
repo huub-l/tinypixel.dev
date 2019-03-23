88229094e9d524849af23c4de280b0cec1cfa523
Kelly Mears <developers@tinypixel.io>
2019-03-23 04:03:37 -0700

    fixes

--- site/web/app/themes/kappakappa/resources/views/404.blade.php
+++ site/web/app/themes/kappakappa/resources/views/404.blade.php
@@ -4,9 +4,11 @@
   @include('partials.page-header')
 
   @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
-    </div>
+    <p class="center-text error-text-help-first"><strong>Oops!</strong></p>
+    <p class="center-text error-text-help-second">The page you were looking for could not be found.</p>
+    <p class="center-text error-text-404">404</p>
+    <div class="center-text error-search-holder">{!! get_search_form(false) !!}</div>
+    <p class="center-text error-text-home">... or return to <a href="{!! $site_url !!}">the front page</a>.</p>
     {!! get_search_form(false) !!}
   @endif
 @endsection
