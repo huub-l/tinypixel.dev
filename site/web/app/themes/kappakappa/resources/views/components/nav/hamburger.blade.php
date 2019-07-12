20335e6dfa9ced8dfe1df7a386e3be5a3fbd4c4d
Kelly Mears <developers@tinypixel.dev>
2019-07-12 00:05:01 -0700

    remove old theme

--- site/web/app/themes/kappakappa/resources/views/components/nav/hamburger.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="toggle-holder">
-  <div id="toggle" class="">
-    <div class="first-menu-line"></div>
-    <div class="second-menu-line"></div>
-    <div class="third-menu-line"></div>
-  </div>
-</div>
--- site/web/app/themes/kappakappa/resources/views/partials/content-page.blade.php
+++
@@ -1,7 +0,0 @@
-
-@php the_content() @endphp
-@php wp_link_pages([
-    'before' => '<p class="wp-link-pages top-50"><span>'. esc_html__('Pages:', 'kapena-wp') . '</span>',
-    'after' => '</p>'
-  ])
-@endphp
