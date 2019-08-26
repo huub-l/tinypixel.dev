11ccc95424b55d68c7d8c0ac3adba97286be8f20
Kelly Mears <developers@tinypixel.dev>
2019-08-26 02:34:16 -0700

    try git factory

--- site/web/app/themes/sage/app/Composers/Plugin.php
+++ site/web/app/themes/sage/app/Composers/Plugin.php
@@ -60,7 +60,9 @@ class Plugin extends Composer
          * Github API Service
          * @var \GrahamCampbell\GitHub\GithubManager
          */
-        $this->git = $app->make('github');
+        $this->git = $app->make('github.factory');
+
+        dd($this);
 
         /**
          * Markdown
